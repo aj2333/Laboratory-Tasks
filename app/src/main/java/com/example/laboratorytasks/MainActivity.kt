548fa3a9786224bd6fedb9747860a9871aa0a8cf
AJ Bataller <abataller0@gmail.com>
2025-10-21 00:03:48 +0800

    Save Code for Exercise-4

--- app/src/main/java/com/example/laboratorytasks/MainActivity.kt
+++ app/src/main/java/com/example/laboratorytasks/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.laboratorytasks
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +14,22 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnStandard: Button = findViewById(R.id.btnStandard)
+        val btnRing: ImageButton = findViewById(R.id.btnRing)
+        val btnToggle: ToggleButton = findViewById(R.id.btnToggle)
+
+        btnToggle.setOnClickListener {
+            val state = if (btnToggle.isChecked) "ON" else "OFF"
+            Toast.makeText(this, "THE BUTTON IS $state", Toast.LENGTH_SHORT).show()
+        }
+
+        btnRing.setOnClickListener {
+            Toast.makeText(this, "THIS BUTTON IS RINGING", Toast.LENGTH_SHORT).show()
+        }
+
+        btnStandard.setOnClickListener {
+            Toast.makeText(this, "THIS BUTTON PERFORMS ADDITION", Toast.LENGTH_SHORT).show()
+        }
     }
 }
