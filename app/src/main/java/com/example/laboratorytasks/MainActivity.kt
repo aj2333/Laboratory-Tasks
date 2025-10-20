5792ea717011ae678fea4660625426f701a7103d
AJ Bataller <abataller0@gmail.com>
2025-10-20 22:08:32 +0800

    Save code for Exercise-2

--- app/src/main/java/com/example/laboratorytasks/MainActivity.kt
+++ app/src/main/java/com/example/laboratorytasks/MainActivity.kt
@@ -10,7 +10,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity(){
 
     private val country = arrayOf("Philippines", "Panama", "Japan", "Canada", "Australia", "United States")
     private val option = arrayOf("Country", "Philippines", "Panama", "Japan", "Canada", "Australia", "United States")
@@ -19,35 +19,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //    insets
-        //val etFullname: EditText = findViewById(R.id.etFullname)
 
-        val actvCountry: AutoCompleteTextView = findViewById(R.id.actvCountry)
-        val countryAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, country)
-        actvCountry.setAdapter(countryAdapter)
-        actvCountry.threshold = 1
 
-        val spinner: Spinner = findViewById(R.id.spnOptions)
-        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, option)
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = spinnerAdapter
-        spinner.onItemSelectedListener = this
-    }
 
-    override fun onItemSelected(
-        parent: AdapterView<*>?,
-        view: View?,
-        position: Int,
-        id: Long
-    ) {
-        val selectedItem = parent?.getItemAtPosition(position).toString()
-        Toast.makeText(this, "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("Not yet implemented")
     }
 }
