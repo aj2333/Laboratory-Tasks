fb5b27fe3ed11c524851c7fd4bf16792bb5a018a
AJ Bataller <abataller0@gmail.com>
2025-10-20 22:18:19 +0800

    Save code for Exercise-2

--- app/src/main/java/com/example/laboratorytasks/MainActivity.kt
+++ app/src/main/java/com/example/laboratorytasks/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.example.laboratorytasks
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Spinner
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
 
-    private val country = arrayOf("Philippines", "Panama", "Japan", "Canada", "Australia", "United States")
-    private val option = arrayOf("Country", "Philippines", "Panama", "Japan", "Canada", "Australia", "United States")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-
-
     }
 }
