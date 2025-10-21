c2c591c7df8c4043c57741c41f02192d66444c65
AJ Bataller <159677564+aj2333@users.noreply.github.com>
2025-10-21 20:51:47 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/laboratorytasks/MainActivity.kt
+++ app/src/main/java/com/example/laboratorytasks/MainActivity.kt
@@ -19,11 +19,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //    insets
-        //val etFullname: EditText = findViewById(R.id.etFullname)
 
         val actvCountry: AutoCompleteTextView = findViewById(R.id.actvCountry)
         val countryAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, country)
