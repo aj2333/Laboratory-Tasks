14a24cc44eccf01cff113a38ae8b07d64cff075c
AJ Bataller <abataller0@gmail.com>
2025-10-21 20:53:42 +0800

    Save Code for Exercise 5

--- app/src/main/java/com/example/laboratorytasks/MainActivity.kt
+++ app/src/main/java/com/example/laboratorytasks/MainActivity.kt
@@ -1,35 +1,51 @@
 package com.example.laboratorytasks
 
+import android.widget.FrameLayout
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageSwitcher
+import android.widget.ImageView
 import android.widget.Toast
 import android.widget.ToggleButton
+import android.widget.ViewSwitcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
+
+    private var img = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val btnStandard: Button = findViewById(R.id.btnStandard)
-        val btnRing: ImageButton = findViewById(R.id.btnRing)
-        val btnToggle: ToggleButton = findViewById(R.id.btnToggle)
+        var imageSwitcher: ImageSwitcher = findViewById(R.id.lift)
+        var btnSwitch: Button = findViewById(R.id.btnSwitch)
 
-        btnToggle.setOnClickListener {
-            val state = if (btnToggle.isChecked) "ON" else "OFF"
-            Toast.makeText(this, "THE BUTTON IS $state", Toast.LENGTH_SHORT).show()
-        }
+        imageSwitcher.setFactory(ViewSwitcher.ViewFactory {
+            val imageView = ImageView(applicationContext)
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+            imageView
+        })
 
-        btnRing.setOnClickListener {
-            Toast.makeText(this, "THIS BUTTON IS RINGING", Toast.LENGTH_SHORT).show()
-        }
+        imageSwitcher.setImageResource(R.drawable.lift)
 
-        btnStandard.setOnClickListener {
-            Toast.makeText(this, "THIS BUTTON PERFORMS ADDITION", Toast.LENGTH_SHORT).show()
+        btnSwitch.setOnClickListener {
+            if(img) {
+                imageSwitcher.setImageResource(R.drawable.lift2)
+                Toast.makeText(this, "Image2", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                imageSwitcher.setImageResource(R.drawable.lift)
+                Toast.makeText(this, "Image1", Toast.LENGTH_SHORT).show()
+            }
+            img = !img
         }
     }
 }
